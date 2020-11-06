ff7afaf42faa190e9ebe16d496e8121c759fdef6
f3rcode <mambrll.frm@gmail.com>
2020-11-06 15:17:49 +0100

    perf: code end fermentation time
    resolves #4

--- incubator/incubator.ino
+++ incubator/incubator.ino
@@ -17,24 +17,27 @@
 #define ERROR_TEMP 1 
 #define ERROR_EXOTHERMIC_TEMP 2
 
+//Time (unsigned long range is from 0 to 4,294,967,295 (2^32 - 1))
+#define ONE_DAY 86400000
+#define TWO_DAYS 172800000
+#define THREE_DAYS 259200000
+
 boolean thresholdFlag= false;
 boolean decreasingFlag= false;
+boolean on= true;
 
 //Debug
 boolean Debugging=false;
 
 
 float Setpoint=TEMPEH;
-float initStop=2; //first of all, it stops warming 2degrees below setpoint (in theory, by inerce it arrives the Setpoint¿)
+float initStop=2; //first of all, it stops warming 3degrees below setpoint (in theory, by inerce it arrives the Setpoint¿)
 float maxError=1; //it adjust when it's 1 degree below
 float threshold=initStop;
 byte relayOutput=0;
 
 //Temporal Variables
-const long timeInterval=2000;
-unsigned long now;
-unsigned long lastMeasure=0;//to store last measure time
-
+int blinkingTime=200;
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -51,65 +54,76 @@ void setup() {
   pinMode(WARNING_LED, OUTPUT);
   digitalWrite(WARNING_LED, LOW);
 
-  
   dht.begin();
- // Serial.println("Begining!:");
   delay(500);
 }
 
 void loop() {
 
-    //-------Sensor reading------
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-  
-    if (isnan(t) || isnan(h)) {
-      Serial.println("Failed to read from DHT");
-    } else { 
-   
-    
-     if (t>(Setpoint-threshold)) {
 
-        //it only executes once: first time temperature gets higher than Setpoint-initStop
-        if ((t>(Setpoint-ERROR_TEMP))&&(!thresholdFlag)){
-            threshold=ERROR_TEMP;
-            thresholdFlag=true; 
-        }
+    if (millis()>ONE_DAY) //in case of TEMPEH fermentation
+      on=false;
+      
 
-        /*thought to be executed when temperature gets higher than warning value
-       when exothermic reaction starts to happen.
-        */
-        if ((t> WARNING_TEMPEH)&&(thresholdFlag)){
-          threshold=ERROR_EXOTHERMIC_TEMP;
-          digitalWrite(WARNING_LED, HIGH);
-          }
+    if (on){
+
+          //-------Sensor reading------
+          float h = dht.readHumidity();
+          float t = dht.readTemperature();
         
-        Serial.println("stop it!");
-        relayOutput=0;
-        digitalWrite(RELAYPIN, LOW);
-                         
-       }
-     else{
+          if (isnan(t) || isnan(h)) {
+            Serial.println("Failed to read from DHT");
+          } else { 
+       
         
-          Serial.println("warm it!");
-          relayOutput=1;
-          digitalWrite(RELAYPIN, HIGH);       
+         if (t>(Setpoint-threshold)) {
+    
+            //it only executes once: first time temperature gets higher than Setpoint-initStop
+            if ((t>(Setpoint-ERROR_TEMP))&&(!thresholdFlag)){
+                threshold=ERROR_TEMP;
+                thresholdFlag=true; 
+            }
+    
+            /*thought to be executed when temperature gets higher than warning value
+           when exothermic reaction starts to happen.
+            */
+            if ((t> WARNING_TEMPEH)&&(thresholdFlag)){
+              threshold=ERROR_EXOTHERMIC_TEMP;
+              digitalWrite(WARNING_LED, HIGH);
+              }
+            
+            Serial.println("stop it!");
+            relayOutput=0;
+            digitalWrite(RELAYPIN, LOW);
+                             
+           }
+         else{
+            
+              Serial.println("warm it!");
+              relayOutput=1;
+              digitalWrite(RELAYPIN, HIGH);       
+            }
+    
+     
+        
+          Serial.print(t);
+          Serial.print(':');
+          Serial.print(relayOutput);
+          Serial.println(':');
+    
+      
         }
-
- 
+        //---------Blinking internal led so I can see it's working--------
     
-      Serial.print(t);
-      Serial.print(':');
-      Serial.print(relayOutput);
-      Serial.println(':');
+          digitalWrite(LED, HIGH); 
+          delay(blinkingTime);              
+          digitalWrite(LED, LOW);    
+          delay(blinkingTime); 
 
-  //---------Blinking internal led so I can see it's working--------
-  }
     
-  digitalWrite(LED, HIGH); 
-  delay(200);              
-  digitalWrite(LED, LOW);    
-  delay(200);   
-  //-------------------------------
+    }else{ //if fermentation is beyond its limit time
+        digitalWrite(LED, LOW);    
+        delay(blinkingTime); 
+    }
 
 }
