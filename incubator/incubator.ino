eaeed6cf81ef52cb7acd462be9f774e1874d07ad
Rhizopus <rhizopus@rhizopus.home>
2021-09-18 14:27:42 +0200

    feat: update menu to LCDMenu
    resolves #6

--- incubator/incubator.ino
+++ incubator/incubator.ino
@@ -1,7 +1,7 @@
 #include <AutoPID.h>
 #include <RBDdimmer.h>
 #include "DHT.h"
-#include <SerialMenu.hpp>
+#include <LCDMenu.hpp>
 
 #define SERIALMENU_MINIMAL_FOOTPRINT true
 
@@ -22,7 +22,7 @@
 
 //LEDS
 #define LED 13 //led interno
-#define WARNING_LED 9
+#define WARNING_LED 5
 
 //Temperature
 #define TEMPEH 31// 28-32 Celsius degrees (max 33 (or 35?)!! CHECK)
@@ -39,6 +39,7 @@
 #define TWO_DAYS 172800000
 #define THREE_DAYS 259200000
 #define HOURS2MS 3600000
+#define HOURS2CONFIG 36
 
 #define BLINKING_TIME 900
 #define INIT_STOP 2; //first of all, it stops warming 2degrees below setpoint (in theory, by inerce it arrives the setpoint)
@@ -47,6 +48,7 @@
 
 //Debug
 boolean debugging=true;
+boolean dhtError=false;
 
 float t=0;
 float h=0;
@@ -61,39 +63,39 @@ unsigned long timeLimit=0;
 
 void fermentLoop();
 
-const SerialMenu& menu = SerialMenu::get();
+LCDMenu& menu = LCDMenu::get();
 
 ///////////////////////////////////////////////////////////////////////////////
 // Main menu
 ///////////////////////////////////////////////////////////////////////////////
 
 
-const char mainMenuTempeh[] PROGMEM = "1 - Tempeh";
-const char mainMenuNatto[] PROGMEM = "2 - Natto";
-const char mainMenuKoji[] PROGMEM = "3 - Koji";
-const char mainMenuConfig[] PROGMEM = "C - Config";
-const char mainMenuMenu[] PROGMEM = "M - Menu";
+char mainMenuTempeh[]  = "1 - Tempeh";
+char mainMenuNatto[]  = "2 - Natto";
+char mainMenuKoji[]  = "3 - Koji";
+char mainMenuConfig[]  = "C - Config";
+char mainMenuMenu[]  = "M - Menu";
 
 
 // Forward declarations for the config-menu referenced before it is defined.
-extern const SerialMenuEntry configMenu[];
+extern LCDMenuEntry configMenu[];
 extern const uint8_t configMenuSize;
 
 // Define the main menu
-const SerialMenuEntry mainMenu[] = {
+LCDMenuEntry mainMenu[] = {
   {mainMenuTempeh, true, '1', [](){ setpoint=TEMPEH;  
                                     timeLimit=ONE_DAY;
                                     warningTemperature=WARNING_TEMPEH;
-                                    Serial.println("Go!");
+                                    menu.print("Go!");
                                     fermentLoop();
                                   } },
   {mainMenuNatto, true, '2', [](){setpoint=NATTO;  
                                   timeLimit=ONE_DAY;
                                   warningTemperature=WARNING_NATTO;
-                                  Serial.println("Go!");
+                                  menu.print("Go!");
                                   fermentLoop();
                                   } },
-  {mainMenuKoji, true, '3', [](){ Serial.println("Still not available.");
+  {mainMenuKoji, true, '3', [](){ menu.print("Still not available.",(uint8_t) 1000);
                                   menu.show();} },
   {mainMenuConfig, true, 'C', [](){ menu.load(configMenu, configMenuSize); menu.show();} },
   {mainMenuMenu, true, 'M', [](){ menu.show(); } },
@@ -105,19 +107,19 @@ constexpr uint8_t mainMenuSize = GET_MENU_SIZE(mainMenu);
 // Config-menu
 ///////////////////////////////////////////////////////////////////////////////
 
-const char configMenuTemp[] PROGMEM = "T - Temperature:";
-const char configMenuTime[] PROGMEM = "H - Time:";
-const char configMenuStart[] PROGMEM = "! - Go!";
-const char configMenuMenu[] PROGMEM = "M - Menu";
-const char configMenuBack[] PROGMEM = "< - Main Menu";
+char configMenuTemp[]  = "T - Temperature:";
+char configMenuTime[]  = "H - Time:";
+char configMenuStart[]  = "! - Go!";
+char configMenuMenu[]  = "M - Menu";
+char configMenuBack[]  = "< - Main Menu";
 
 
 // Define the config-menu
-const SerialMenuEntry configMenu[] = {
-  {configMenuTemp, true, 'T', [](){ setpoint = menu.getNumber<float>("Temp: ");
+LCDMenuEntry configMenu[] = {
+  {configMenuTemp, true, 'T', [](){ setpoint = menu.getNumber<float>("Temp: ", TEMPEH);
                                     warningTemperature = setpoint + ERROR_TEMP;
                                     menu.show();} },
-  {configMenuTime, true, 'H', [](){ timeLimit = HOURS2MS * menu.getNumber<int>("Time (hours): "); 
+  {configMenuTime, true, 'H', [](){ timeLimit = HOURS2MS * menu.getNumber<int>("Time (hours): ",HOURS2CONFIG); 
                                     menu.show();} }, //SHOULD LET USE PARTS OF AN HOUR AS A VALUE!
   {configMenuStart, true, '!',[](){ if ((setpoint==0) || (timeLimit==0)){
                                         Serial.println("You need to set temp & time!");
@@ -155,6 +157,7 @@ void setup() {
   pinMode(WARNING_LED, OUTPUT);
   digitalWrite(WARNING_LED, LOW);
 
+  menu.lcdBegin();
   // Install mainMenu as the current menu to run
   menu.load(mainMenu, mainMenuSize);
   // Display current menu (mainMenu)
@@ -196,6 +199,7 @@ boolean sensorReading(float* h,float* t){
     
   if (isnan(*t) || isnan(*h)) {
          Serial.println("Failed to read from DHT");
+         dhtError=true;
          return false;
   }
 
@@ -217,8 +221,13 @@ boolean bypassingDimmerSensorReading(float* h,float* t, float* out){
   return sensorRead;
 }
 
-void printSensorActuatorValues(float* output){
+void printSensorActuatorValues(float* output, boolean serial){
 
+  if (!serial)
+    menu.print(t,h,*output);
+
+  if (dhtError)
+    menu.print("DHT error :(","u better shut down");
   
   if (debugging){
     Serial.print(t);
@@ -250,20 +259,20 @@ void fermentLoop(){
 
     while(on){
       
-          if ((millis()-startingTime)>timeLimit){ 
-             on=false;
-                
-          if (bypassingDimmerSensorReading(&h,&t,&outputDimmer)){ 
-       
-            warmDimmerAlgorithm();
-                
-            blinkLed();
-            
-            printSensorActuatorValues(&outputDimmer);
+        if ((millis()-startingTime)>timeLimit) 
+           on=false;
+              
+        if (bypassingDimmerSensorReading(&h,&t,&outputDimmer)){ 
+     
+          warmDimmerAlgorithm();
+              
+          blinkLed();
+          
+          printSensorActuatorValues(&outputDimmer,false);
+  
+       }
 
-        }
-
-       
+       delay(1000);
        
     }//if fermentation is beyond its limit time
           
@@ -272,18 +281,21 @@ void fermentLoop(){
       
     digitalWrite(LED, LOW);    
 
-    return; 
+    return;       
     
 }
 
   
 void loop() {
+    
+    menu.run(700);
 
-   menu.run(500);
-
-   if(bypassingDimmerSensorReading(&h,&t,&outputDimmer))
-    printSensorActuatorValues(&outputDimmer );
-
+    if (debugging){
+       if(bypassingDimmerSensorReading(&h,&t,&outputDimmer))
+        printSensorActuatorValues(&outputDimmer,true);
+       delay(1000);
+    }
+    
   
-   delay(1000);
+   //
 }
